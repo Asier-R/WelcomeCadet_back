9e89b147431e9986864e0cf4a4e0396a879427ea
asier.virtualbox <asier.virtualbox@prueba.com>
2023-08-17 12:27:05 +0200

    Primer commit

---
+++ src/main/java/com/welcomecadet/welcomecadet/WelcomecadetApplication.java
@@ -0,0 +1,13 @@
+package com.welcomecadet.welcomecadet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomecadetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomecadetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/welcomecadet/welcomecadet/WelcomecadetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welcomecadet.welcomecadet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomecadetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
