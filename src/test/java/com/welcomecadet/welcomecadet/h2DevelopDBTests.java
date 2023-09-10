4b05318c6bc47c667cf9b5a3c3bb0909752b0a19
Asier <monstruosaurio@gmail.com>
2023-09-10 13:21:18 +0200

    Se añade base de datos h2 con tabla y datos de prueba.

---
+++ src/test/java/com/welcomecadet/welcomecadet/h2DevelopDBTests.java
@@ -0,0 +1,10 @@
+package com.welcomecadet.welcomecadet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class h2DevelopDBTests {
+
+
+}
